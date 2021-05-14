4402b7b0468278dd8c3ba81479e82d9e687d1fdd
AntonTuzov <antontuzovf@gmail.com>
2021-05-14 09:44:06 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Anton Tuzov on 14.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
